1ddf9b40fa93900a5dd3a2617ca3d2d4d45ab3c1
Georgi Spasov <georgi.spasov@outlook.com>
2016-10-23 01:49:21 +0300

    Java Testing

---
+++ JavaTesting/src/javatesting/JavaTesting.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatesting;
+
+/**
+ *
+ * @author Shadow
+ */
+public class JavaTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
