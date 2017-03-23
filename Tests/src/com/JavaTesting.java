cb5ef87f2c338fb7c88abebaa52b13078306c504
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-23 02:25:29 +0200

    Revision

--- Tests/src/com/JavaTesting.java
+++ Tests/src/com/JavaTesting.java
@@ -1,33 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Shadow
- */
 public class JavaTesting {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //System.out.println("Welcome\nto\nJava\nProgramming!");
-        System.out.printf("%s\n%s\n", "Welcome to", "Java Programming!");
-        
-        
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Enter number:");
-        int number = input.nextInt();
-        System.out.println(number);
-        
-    }
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        double num = input.nextDouble();
+        System.out.printf("Square result: %f.\t", Math.pow(num, 2));
+    }
 }
