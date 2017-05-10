f551300f5ef29cbaf7a67356d3ba325738bfa1ff
Georgi Spasov <georgi.spasov@outlook.com>
2017-05-10 16:06:35 +0300

    Decoupling lambda expression from method implementation v.2

--- Tests/src/com/basics/lambdas/Lambdas.java
+++ Tests/src/com/basics/lambdas/Lambdas.java
@@ -6,6 +6,14 @@ public class Lambdas {
         return String.format("The result is: " + a + b);
     };
 
+    private static IAddable<String> leghtOrder = (a, b) -> {
+        if (a.length() > b.length()) {
+            return String.format("The result is: " + a + b);
+        } else {
+            return String.format("The result is: " + b + a);
+        }
+    };
+
     private static void applyBehaviour(IAddable addable, String s1, String s2) {
         System.out.println(addable.add(s1, s2));
     }
@@ -13,8 +21,9 @@ public class Lambdas {
     public static void main(String[] args) {
 
         String s1 = "This is ";
-        String s2 = "a test!";
+        String s2 = "a test!!!";
 
         applyBehaviour(stringAdder, s1, s2);
+        applyBehaviour(leghtOrder, s1, s2);
     }
 }
