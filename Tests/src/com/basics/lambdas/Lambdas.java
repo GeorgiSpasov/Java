2dbc6218b6a40abe237da23e9a2225953ce1d84a
Georgi Spasov <georgi.spasov@outlook.com>
2017-05-16 21:31:56 +0300

    Stream Celsius to Fahrenheit Converter

--- Tests/src/com/basics/lambdas/Lambdas.java
+++ Tests/src/com/basics/lambdas/Lambdas.java
@@ -1,6 +1,9 @@
 package com.basics.lambdas;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Lambdas {
 
@@ -20,23 +23,36 @@ public class Lambdas {
         }
     };
 
-    private static void applyBehaviour(IAddable addable, String s1, String s2) {
+    public static void applyBehaviour(IAddable addable, String s1, String s2) {
         System.out.println(addable.add(s1, s2));
     }
 
     public static void main(String[] args) {
         System.out.println(stringAdder.add("hi ", "there"));
 
+        // Decouple functionality from method implementation
         String s1 = "this is ";
         String s2 = "a new test ";
         applyBehaviour(stringAdder, s1, s2);
         applyBehaviour(leghtOrder, s1, s2);
 
-        int intArray[] = {5, 3, 4, 7, 2, 6};
-        Arrays.stream(intArray)
+        // Use Predicate to sort and BinaryOperator to sum
+        Integer intArray[] = {5, 3, 4, 7, 2, 6};
+        List<Integer> r = Arrays.stream(intArray)
                 .filter(i -> i > 4)
                 .sorted()
-                .map(i -> i * i)
-                .forEach(System.out::println);
+                .map(i -> i + 10)
+                .collect(Collectors.toList());
+
+        // Convert Celsius list to Fahrenheit
+        Function<List<Integer>, List<Double>> convertCelsius
+                = i -> i.stream()
+                        .map(x -> (x * 9.0 / 5 + 32))
+                        .collect(Collectors.toList());
+
+        List<Double> fTemps = convertCelsius.apply(r);
+
+        System.out.println(r);
+        System.out.println(fTemps);
     }
 }
